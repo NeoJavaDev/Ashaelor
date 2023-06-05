2d54ad9f14d81905b7f22ec407e93d50947f9ead
Neo <neo.javadev@gmail.com>
2023-06-05 20:27:05 +0200

    LibGdx Structure

---
+++ desktop/src/com/neojavadev/ashaelor/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.neojavadev.ashaelor;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.neojavadev.ashaelor.AshAelor;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("AshAelor");
+		new Lwjgl3Application(new AshAelor(), config);
+	}
+}
